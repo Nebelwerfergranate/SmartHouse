275a8c888b0b7a005fa004cc3cc0eefbcf144258
Nebelwerfergranate <acht-acht@mail.ru>
2015-09-30 23:10:19 +0300

    включил 2 забытых файла исходного кода

---
+++ SmartHome/Devices/FridgeModule.cs
@@ -0,0 +1,52 @@
+﻿namespace SmartHome
+{
+    public abstract class FridgeModule : Device, IOpenable, ITemperaturable, IVolume
+    {
+        // Fields
+        protected readonly double volume;
+
+        protected bool isOpen;
+
+        protected double temperature;
+
+
+        // Constructors
+        protected FridgeModule(uint volume)
+        {
+            if (volume < 10)
+            {
+                this.volume = 10;
+            }
+            else
+            {
+                this.volume = volume;
+            }
+        }
+
+
+        // Properties
+        public bool IsOpen
+        {
+            get { return isOpen; }
+        }
+
+        public abstract double Temperature
+        { get; set; }
+
+        public double Volume
+        {
+            get { return volume; }
+        }
+
+
+        // Methods
+        public virtual void Close()
+        {
+            isOpen = false;
+        }
+        public virtual void Open()
+        {
+            isOpen = true;
+        }
+    }
+}
---
+++ SmartHome/Devices/Lamp.cs
@@ -0,0 +1,26 @@
+﻿namespace SmartHome
+{
+    public class Lamp : SmartHome.Device
+    {
+        // Fields
+        // Лампочки с нулевой мощностью не смогут подсвечивать.
+        private readonly double power = 1;
+
+
+        // Constructors
+        public Lamp(double power)
+        {
+            if (power > 1)
+            {
+                this.power = power;
+            }
+        }
+
+
+        // Properties
+        public double Power
+        {
+            get { return power; }
+        }
+    }
+}
